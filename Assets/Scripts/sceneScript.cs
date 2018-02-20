5e268b80f7b3bb29f1deaedd965ae9a4b3c7a92e
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-02-20 04:56:58 -0500

    Added an FPS counter and fixed UI issues
    Prevented UI elements from changing during gameplay, and added an FPS
    counter so we can get FPS in the built version of the game.

--- Assets/Scripts/sceneScript.cs
+++ Assets/Scripts/sceneScript.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sceneScript : MonoBehaviour {
 
     public Canvas gameOptionsUICanvas;
     public cameraController cam;
+    public Slider sensitivitySlider;
+    public Toggle mouseAxis;
+    public Toggle fpsShowToggle;
+    public Text fpsDisplayer;
 
     private void Start()
     {
@@ -17,16 +22,30 @@ public class sceneScript : MonoBehaviour {
         if (Input.GetKeyDown("escape")) {
             if (gameOptionsUICanvas.enabled == false) {
                 gameOptionsUICanvas.enabled = true;
+                sensitivitySlider.interactable = true;
+                mouseAxis.interactable = true;
+                fpsShowToggle.interactable = true;
                 Cursor.visible = true;
                 cam.Pause();
                 Time.timeScale = 0;
             }
             else {
                 gameOptionsUICanvas.enabled = false;
+                sensitivitySlider.interactable = false; // makes sure these can't be changed after escaping the UI
+                mouseAxis.interactable = false;
+                fpsShowToggle.interactable = false;
                 Cursor.visible = false;
                 cam.Restart();
                 Time.timeScale = 1;
             }
         }
+        if (fpsShowToggle.isOn == true && Time.timeScale != 0)
+        {
+            fpsDisplayer.text = "FPS\n" + (Mathf.Round(1 / Time.smoothDeltaTime)).ToString();
+        }
 	}
+
+    public void ToggleFPSDisplayer() {
+        fpsDisplayer.enabled = fpsShowToggle.isOn;
+    }
 }
