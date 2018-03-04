bc5d350e1b63b7b16615d6de86666f30c7a89f6a
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-03-03 22:08:35 -0500

    Added an objective type with child objectives This can be used with the objective manager to easily create, destroy and maintain in-game objectives.

---
+++ Assets/Scripts/objective.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objective {
+
+    public int serialNumber;
+    public string title;
+    public string description;
+    public string hint;
+    public float completionLevel;
+
+    List<childObjective> childObjectives = new List<childObjective>();
+
+    public objective(string titleParam, string descriptionParam, float completionLevelParam, string hintParam, objective[] childObjectives) {
+
+        title = titleParam;
+        description = descriptionParam;
+        completionLevel = completionLevelParam;
+        if (hintParam != null) {
+            hint = hintParam; // not necessary to define a hint
+        }
+
+        if (childObjectives != null) { // not necessary to have any child objectives
+            foreach (objective child in childObjectives)
+            {
+                MakeChild(child);
+            }
+        }
+    }
+
+    private void MakeChild(objective child)
+    {
+        int numberOfGrandChildren = child.childObjectives.Count; // a child objective may have grandchildren
+
+        if (numberOfGrandChildren == 0) // if no grandchildren pass null as childObjectives of child
+        {
+            childObjectives.Add(new childObjective(child.title, child.description, child.completionLevel, child.hint, null));
+        }
+        else // else get the list of child's child objectives and pass it as grandChildObjectives
+        {
+            objective[] grandChildObjectives = new objective[numberOfGrandChildren];
+
+            for (int i = 0; i < numberOfGrandChildren; i++)
+            {
+                grandChildObjectives[i] = child.GetChild(i);
+            }
+            // grandChildObjectives will now be re-passed as childObjectives of the child in the constructor of "objective", which calls MakeChild again
+            // and gets the children of the grandchildren recursively till it hits null
+            childObjectives.Add(new childObjective(child.title, child.description, child.completionLevel, child.hint, grandChildObjectives));
+        }
+    }
+
+    private objective GetChild(int serialNumber)
+    {
+        return childObjectives[serialNumber].thisChild; // returns type "objective" from every instance of childObjective in the dynamic list 
+    }
+
+    public bool IsComplete() {
+
+        if (completionLevel == 100)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -70,6 +70,4 @@ public class playerController : MonoBehaviour {
 			offsetController.WalkDirectionSet();
 		}
 	}
-
-   
 }
