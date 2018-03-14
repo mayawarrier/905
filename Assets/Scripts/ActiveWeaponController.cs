8ec5a1670a9acd881fedcd842e2762592ccaec75
Wizdem <argankit@gmail.com>
2018-03-14 18:18:56 +0530

    Added code for weapons1.0

---
+++ Assets/Scripts/ActiveWeaponController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveWeaponController : MonoBehaviour {
+
+	weapon current, primary, secondary, melee, blank;
+	void Start()
+	{
+		//load weapons from save file
+	}
+	void Update()
+	{
+		if (Input.GetButton ("1")) {
+			current = primary;
+		}
+		if (Input.GetButton ("2")) {
+			current = secondary;
+		}
+		if (Input.GetButton ("3")) {
+			current = melee;
+		}
+		if (Input.GetButton ("Fire")) {
+			current.Fire ();
+		}
+		if (Input.GetButton ("G")) {
+			//gun tossing animation
+			current = blank;
+		}
+		if (Input.GetButton ("R")) {
+			current.Reload ();
+		}
+	}
+	void Pickup(weapon pickup)
+	{
+		if (pickup.tag == "primary")
+			primary = pickup;
+		else if (pickup.tag == "secondary")
+			secondary = pickup;
+		else if (pickup.tag == "melee")
+			melee = pickup;
+	}
+
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,49 @@
+﻿//Attach this to the weapon object
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+
+	public int clipCapacity, loadedBullets, extraBullets, bulletsShotAtOnce;
+	public GameObject bullet;
+	public Transform bulletSpawn;
+	public float fireRate, reloadTime;
+	public string tag;
+
+	private AudioSource audioSource, nullAudio;
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+		nullAudio = GetComponent<AudioSource> ();
+	}
+
+	public void Fire ()
+	{
+		if (loadedBullets > 0) {
+			for (int i = 0; i < bulletsShotAtOnce; i++) {
+				Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
+			}
+			loadedBullets -= bulletsShotAtOnce;
+			audioSource.Play ();
+		} else if (tag == "melee") {
+			 //add code to whack the enemy with melee weapon
+		} else {
+			nullAudio.Play ();
+		}
+	}
+	public void Reload ()
+	{
+		if (loadedBullets != clipCapacity) {
+			yield return new WaitForSeconds (reloadTime);
+			if (extraBullets >= clipCapacity - loadedBullets) {
+				extraBullets -= clipCapacity - loadedBullets;
+				loadedBullets = clipCapacity;
+			} else {
+				loadedBullets += extraBullets;
+				extraBullets = 0;
+			}
+		}
+	}
+}
