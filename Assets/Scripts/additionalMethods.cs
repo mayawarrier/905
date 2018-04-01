911145fc1c4453c9043d64f1e811e1faf17bfe78
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-04-01 02:27:57 -0400

    Made an internal library for useful global methods. Other classes should inherit from it.

---
+++ Assets/Scripts/additionalMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class additionalMethods {
+
+    public int charToInt(char character)
+    {
+        return (int)char.GetNumericValue(character);
+    }
+}
