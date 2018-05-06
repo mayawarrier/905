d67529db06ee7a3c812c7af18ecc284a9bd943df
Dhruv Warrier <dhruv.warrier89@gmail.com>
2018-05-05 22:47:42 -0400

    Modified the internal library to include definitions for invalid objectives and valid objective attribute types. Also added an intToBool function that returns true only if int is non-zero

--- Assets/Scripts/internalLib.cs
+++ Assets/Scripts/internalLib.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class internalLib {
+
+    public static bool NOT_FOUND = false;
+    public static objective OBJECTIVE_NOT_FOUND = new objective(NOT_FOUND);
+    public Type[] allowedObjectiveAttrTypes = { typeof(string), typeof(float) };
 
     public int charToInt(char character)
     {
         return (int)char.GetNumericValue(character);
+    }
+
+    public bool intToBool(int integer)
+    {
+        if (integer == 0)
+        {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
